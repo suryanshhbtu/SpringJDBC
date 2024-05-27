c56096005384bc1261aa0a4dfddbfd0d29c772f7
suryanshhbtu <suryanshhbtu@gmail.com>
2024-05-27 14:02:57 +0530

    using jdbcTemplate directly without any structure

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,13 +1,24 @@
 package com.spring.jdbc;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        
+        JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+        
+        //query
+        String query = "insert into student(id, name, city) values (?, ?, ?)";
+        //fire
+        
+        int res = template.update(query, 4, "Shivam", "Pakistan");
+        System.out.println(res);
+        
     }
 }
